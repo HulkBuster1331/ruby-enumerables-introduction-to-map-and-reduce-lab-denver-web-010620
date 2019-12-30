511d42ee4d1f95b2ff99f2b6e9ed5929f675213d
Dillon Franklin <drfranklin1331@gmail.com>
2019-12-30 18:12:00 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -91,7 +91,15 @@ end
  
 
 def reduce_to_any_ture(source_array)
-
- 
+  i = 0 
+    value = 0 
+    
+      while i < source_array.length do
+         return true if source_array[i] == true
+           
+    i += 1
+    end
+  return true
+end
  
 end
